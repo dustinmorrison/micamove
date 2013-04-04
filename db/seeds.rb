02dc3204a1aa92ed45128f1fdb9b0d3de7a4b488
Steve Holm <steveholmphoto@me.com>
2013-04-03 20:08:00 -0500

    continued footer styles and started to seed the products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.destroy_all
+
+products = [
+  { :name => "The Plank", :color => "Black", :description => "", :category => "Watches", :shoplink => "https://mica.infusionsoft.com/app/manageCart/addProduct?productId=119", :price => "185", :image1 => "assets/products/watches/plank-black/plank-black-front.jpg", :image2 => "assets/products/watches/plank-black/plank-black-profile.jpg", :image3 => "assets/products/watches/plank-black/plank-black-belly.jpg"},
+]
+
+Product.create products

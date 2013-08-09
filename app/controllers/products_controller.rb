371d9876c4df4234fcf886a9f319e80eda6fa790
Steve Holm <steveholmphoto@me.com>
2013-08-09 09:02:36 -0600

    fixed text rendering issues, updated support page links, fixed repsonsive visual page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
-    @deck_link = Product.where :name => "The Deck"
+
 
     respond_to do |format|
       format.html # index.html.erb

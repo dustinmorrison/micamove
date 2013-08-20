e49636aa8220d08c153e0b353adb819bfe687886
Steve Holm <steveholmphoto@me.com>
2013-08-19 23:13:43 -0500

    added descriptions to views and fixed bugs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def meta_description(desc = nil)
+    if desc.present?
+      content_for :meta_description, desc
+    else
+      content_for?(:meta_description) ? content_for(:meta_description) : APP_CONFIG['meta_description']
+    end
+  end
+  
 end

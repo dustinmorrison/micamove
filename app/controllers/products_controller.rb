1fd775645c866faab174bbc98c725364dc97e6e0
Kavi Sharma <kaviraaz.net@gmail.com>
2014-06-08 14:18:37 +0530

    out of stock product message added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,7 +18,52 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
+
+    #http://www.micamove.com/products/the-albuquerque-cardinal-tshirt Size XL
+    #http://www.micamove.com/products/the-snake-charmer-black-tshirt Size L
+    #http://www.micamove.com/products/the-movement-black-tshirt Size S and L
+    #http://www.micamove.com/products/the-movement-ash-tshirt Size S
+    #http://www.micamove.com/products/the-tree-midnight-navy-tshirt Size L
+    #http://www.micamove.com/products/the-tree-natural-tshirt Size L and XL
+
+    #http://www.micamove.com/products/the-aztek-charcoal-tshirt Size L
+    #http://www.micamove.com/products/the-aztek-white-tshirt Size M and L
+
+    @xl_na = false
+    @l_na = false
+    @m_na = false
+    @s_na = false
+
     @product = Product.find(params[:id])
+
+    if @product.friendly_id == 'the-albuquerque-cardinal-tshirt'
+        @xl_na = true
+    end
+    if @product.friendly_id == 'the-snake-charmer-black-tshirt'
+        @l_na = true
+    end
+    if @product.friendly_id == 'the-movement-black-tshirt'
+        @s_na = true
+        @l_na = true
+    end
+    if @product.friendly_id == 'the-movement-ash-tshirt'
+        @s_na = true
+    end
+    if @product.friendly_id == 'the-tree-midnight-navy-tshirt'
+        @l_na = true
+    end
+    if @product.friendly_id == 'the-tree-natural-tshirt'
+        @l_na = true
+        @xl_na = true
+    end
+    if @product.friendly_id == 'the-aztek-charcoal-tshirt'
+        @l_na = true
+    end
+    if @product.friendly_id == 'the-aztek-white-tshirt'
+        @m_na = true
+        @l_na = true
+    end
+
     @plank_images = Product.where :name => "The Plank"
     @deck_images = Product.where :name => "The Deck"
     @drift_images = Product.where :name => "The Drift"
@@ -103,9 +148,9 @@ class ProductsController < ApplicationController
 
   def clothing
     if ENV['CANONICAL_HOST']
-      @clothing = Product.all(:conditions =>  ["id > 358 and category = 'Clothing'"])
+      @clothing = Product.all(:conditions => ["id > 358 and category = 'Clothing'"])
     else
-      @clothing = Product.all(:conditions =>  ["id > 15 and category = 'Clothing'"])
+      @clothing = Product.all(:conditions => ["id > 15 and category = 'Clothing'"])
     end
 
 
